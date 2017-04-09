a5fcdad845f5d2a8d24b22dda7c6c835ada1409c
Van <fan543119@gmail.com>
2017-04-09 22:18:40 +0800

    update JS

--- www/qq.js
+++ www/qq.js
@@ -7,14 +7,18 @@ module.exports = {
         QQZone: 1, // QQ 空间
         Favorite: 2  // 收藏
     },
+    ClientType: {
+        QQ:  0, // QQ 手机客户端
+        TIM: 1, // TIM 客户端
+    },
 	ssoLogin:function(successCallback, errorCallback){
-		cordova.exec(successCallback, errorCallback, "QQSDK", "ssoLogin",[]);
+		cordova.exec(successCallback, errorCallback, "QQSDK", "ssoLogin",[args]);
 	},
 	logout:function(successCallback, errorCallback){
 		cordova.exec(successCallback, errorCallback, "QQSDK", "logout", []);
 	},
 	checkClientInstalled:function(successCallback, errorCallback){
-		cordova.exec(successCallback, errorCallback, "QQSDK", "checkClientInstalled", []);
+		cordova.exec(successCallback, errorCallback, "QQSDK", "checkClientInstalled", [args]);
 	},
 	shareText:function(successCallback, errorCallback,args){
 		cordova.exec(successCallback, errorCallback, "QQSDK", "shareText", [args]);
